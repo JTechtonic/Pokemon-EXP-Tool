d3a3189905ad44eeaed73c04865263a9e8c35c0b
JTechtonic <105141152+JTechtonic@users.noreply.github.com>
2022-07-11 03:42:24 -0400

    Finished rest of experience groups and also added a debug function to see if the exp tables are good

--- exp.c
+++ exp.c
@@ -1,17 +1,21 @@
 /**
  * Name: Jeovan Teixeira
- * Last Edited: 7/9/2022
+ * Last Edited: 7/11/2022
  * Purpose: This program is meant to be a helper program that calculates how much experience grinding is left to do for the user's pokemon. Random idea that I had the urge to code cuz why not lol.
- * (UNFINISHED)
  */
 
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 int CalculateTargetEXP(int rate, int targetLevel);
+void Test();
 
 int main()
 {
+    // This test function is to see if the CalculateTargetEXP function makes a correct exp table
+    //Test();
+
     // These integer variables help keep track of where we are in terms of experience
     int targetLevel, currentExp, currentLevel, levelingRate;
     int expGained = 0;
@@ -80,15 +84,49 @@ int main()
 
 /**
  * This function will calculate the target experience we need for a certain level using the different leveling rates pokemon uses. There are 6 total leveling rates in the game all with different functions
- * (UNFINISHED ONLY WORKS FOR SLOW & MEDIUM SLOW EXPERIENCE GROUP)
  */
 int CalculateTargetEXP(int rate, int targetLevel)
 {
     // Variable that will hold our target exp that we will return
     double targetExp = 0;
 
+    if(targetLevel == 1) return 0;
+
     switch(rate)
     {
+        case 0: // Case 0 is for the "erratic" experience group (link to piecewise function: https://bulbapedia.bulbagarden.net/wiki/Experience#Erratic)
+            if(targetLevel < 50)
+            {
+                targetExp = (pow(targetLevel, 3) * (100 - targetLevel)) / 50.0;
+                targetExp = floor(targetExp);
+            }
+            else if(targetLevel >= 50 && targetLevel < 68)
+            {
+                targetExp = (pow(targetLevel, 3) * (150 - targetLevel)) / 100.0;
+                targetExp = floor(targetExp);
+            }
+            else if(targetLevel >= 68 && targetLevel < 98)
+            {
+                targetExp = (pow(targetLevel, 3) * ((1911 - (10 * targetLevel)) / 3)) / 500.0;
+                targetExp = floor(targetExp);
+            }
+            else if(targetLevel >= 98 && targetLevel <= 100)
+            {
+                targetExp = (pow(targetLevel, 3) * (160 - targetLevel)) / 100.0;
+                targetExp = floor(targetExp);
+            }
+            break;
+
+        case 1: // Case 1 is for the "fast" experience group
+            targetExp = (4 * pow(targetLevel, 3)) / 5.0; // (4 * n^3) / 5
+            targetExp = floor(targetExp);
+            break;
+
+        case 2: // Case 2 is for the "medium fast" experience group
+            targetExp = pow(targetLevel, 3); // n^3
+            targetExp = floor(targetExp);
+            break;
+
         case 3: // Case 3 is for the "medium slow" experience group
             targetExp = ((6.0 / 5.0) * pow(targetLevel, 3)) - (15 * pow(targetLevel, 2)) + (100 * targetLevel) - 140; // (6/5 * n^3) - (15 * n^2) + (100 * n) - 140
             targetExp = floor(targetExp);
@@ -98,8 +136,39 @@ int CalculateTargetEXP(int rate, int targetLevel)
             targetExp = (5 * pow(targetLevel, 3)) / 4; // (5 * n^3) / 4
             targetExp = floor(targetExp);
             break;
+
+        case 5: // Case 5 is for the "fluctuating" experience group (link to piecewise function: https://bulbapedia.bulbagarden.net/wiki/Experience#Fluctuating)
+            if(targetLevel < 15)
+            {
+                targetExp = pow(targetLevel, 3) * ((((targetLevel + 1) / 3.0) + 24) / 50.0);
+                targetExp = floor(targetExp);
+            }
+            else if(targetLevel >= 15 && targetLevel < 36)
+            {
+                targetExp = pow(targetLevel, 3) * ((targetLevel + 14) / 50.0);
+                targetExp = floor(targetExp);
+            }
+            else if(targetLevel >= 36 && targetLevel <= 100)
+            {
+                targetExp = pow(targetLevel, 3) * (((targetLevel / 2.0) + 32) / 50.0);
+                targetExp = floor(targetExp);
+            }
+            break;
     }
 
     // Return our data
     return (int)targetExp;
+}
+
+void Test()
+{
+    FILE* output;
+    output = fopen("table.txt", "w");
+
+    for(int i = 1; i <= 100; i++)
+    {
+        fprintf(output, "%-4d: %-8d\t%-8d\t%-8d\t%-8d\t%-8d\t%-8d\n", i, CalculateTargetEXP(0, i), CalculateTargetEXP(1, i), CalculateTargetEXP(2, i), CalculateTargetEXP(3, i), CalculateTargetEXP(4, i), CalculateTargetEXP(5, i));
+    }
+
+    fclose(output);
 }
